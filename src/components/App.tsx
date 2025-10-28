bc3e59d12e808ef8e1421a802f72e0532476a70c
LeBochuliak <lena.bochuliak@gmail.com>
2025-10-28 21:20:17 +0100

    add components and module.css

---
+++ src/components/App.tsx
@@ -0,0 +1,22 @@
+import css from '../css/App.module.css'
+import Product from './Product'
+
+export default function App() {
+  return (
+    <>
+      <h1 className={css.title}>Products</h1>
+
+      <Product
+        name="Tacos With Lime"
+        imgUrl="https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?w=640"
+        price={10.99}
+      />
+      <Product
+        name="Fries and Burger"
+        imgUrl="https://images.pexels.com/photos/70497/pexels-photo-70497.jpeg?w=640"
+        price={14.29}
+      />
+    </>
+  )
+}
+
---
+++ src/components/Product.tsx
@@ -0,0 +1,18 @@
+import css from '../css/Product.module.css';
+
+interface ProductProps {
+  name: string;
+  imgUrl: string;
+  price: number;
+};
+
+
+export default function Product({ name, imgUrl, price }: ProductProps) {
+  return (
+    <div>
+      <h2 className={css.title}>{name}</h2>
+			<img src={imgUrl} alt={name} width="320" />
+			<p>Price: {price} credits</p>
+    </div>
+  );
+}
